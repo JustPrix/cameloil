2150962c9599a4c6d2716c4b7413171d55fd6324
Fernando Ngana <fernandongana@gmail.com>
2020-11-13 20:11:04 +0200

    Updates

---
+++ precache-manifest.a065bda268a94cd4f10b255d448addd8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0435f3e94d1761b9b96e782c98d9200",
+    "url": "/cameloil/index.html"
+  },
+  {
+    "revision": "d3011e67e15786b86878",
+    "url": "/cameloil/static/css/2.6e90b12a.chunk.css"
+  },
+  {
+    "revision": "aa8efd64ffbf206cdcdf",
+    "url": "/cameloil/static/css/main.11bfb114.chunk.css"
+  },
+  {
+    "revision": "d3011e67e15786b86878",
+    "url": "/cameloil/static/js/2.b0fbe128.chunk.js"
+  },
+  {
+    "revision": "9c4a88d9b57cf919f2c23e04e5eabae2",
+    "url": "/cameloil/static/js/2.b0fbe128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8efd64ffbf206cdcdf",
+    "url": "/cameloil/static/js/main.001813d7.chunk.js"
+  },
+  {
+    "revision": "c5eafabe4b2314660f3de33fe2c9484b",
+    "url": "/cameloil/static/js/main.001813d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256ddc8b6463c95ad4c1",
+    "url": "/cameloil/static/js/runtime-main.ef5d07d7.js"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/cameloil/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/cameloil/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/cameloil/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/cameloil/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
